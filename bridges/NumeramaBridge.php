6e7b26668e0fba6608d2e4f952737d460b7f6047
pit-fgfjiudghdf <github@morlon.org>
2014-03-12 14:22:07 +0100

    Create NumeramaBridge.php

--- bridges/NumeramaBridge.php
+++ bridges/NumeramaBridge.php
@@ -11,31 +11,32 @@ class NumeramaBridge extends BridgeAbstract{
     public function collectData(array $param){
 
     function NumeramaStripCDATA($string) {
-    	$string = str_replace('<![CDATA[', '', $string);
-    	$string = str_replace(']]>', '', $string);
-    	return $string;
+        $string = str_replace('<![CDATA[', '', $string);
+        $string = str_replace(']]>', '', $string);
+        return $string;
     }
     function NumeramaExtractContent($url) {
-	$html2 = file_get_html($url);
-	$text = $html2->find('h2.intro', 0)->innertext;
-	$text = $text.$html2->find('div.content', 0)->innertext;
-	return $text;
+        $html2 = file_get_html($url);
+        $text = $html2->find('h2.intro', 0)->innertext;
+        $text = $text.$html2->find('div.content', 0)->innertext;
+        $text = preg_replace('/<script .*>/i','',$text); //Supression des balises script apparentes
+        return $text;
     }
         $html = file_get_html('http://www.numerama.com/rss/news.rss') or $this->returnError('Could not request Numerama.', 404);
-	$limit = 0;
+        $limit = 0;
 
-	foreach($html->find('item') as $element) {
-	 if($limit < 5) {
-	 $item = new \Item();
-	 $item->title = NumeramaStripCDATA($element->find('title', 0)->innertext);
-	 $item->uri = NumeramaStripCDATA($element->find('guid', 0)->plaintext);
-	 $item->timestamp = strtotime($element->find('pubDate', 0)->plaintext);
-	 $item->content = NumeramaExtractContent($item->uri);
-	 $this->items[] = $item;
-	 $limit++;
-	 }
-	}
-    
+        foreach($html->find('item') as $element) {
+         if($limit < 5) {
+         $item = new \Item();
+         $item->title = NumeramaStripCDATA($element->find('title', 0)->innertext);
+         $item->uri = NumeramaStripCDATA($element->find('guid', 0)->plaintext);
+         $item->timestamp = strtotime($element->find('pubDate', 0)->plaintext);
+         $item->content = NumeramaExtractContent($item->uri);
+         $this->items[] = $item;
+         $limit++;
+         }
+        }
+
     }
 
     public function getName(){
